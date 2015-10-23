a0b665a585a7dbf6431cc2816e6714c421c6d20a
Akhil Chaudhari <akhil.chaudhari91@live.com>
2015-10-23 19:02:10 +0530

    Updated the DB Context with new entities and removed the sample Person and User class references

--- UnitOfWorkApplication.Model/Entities/UnitOfWorkApplicationDBContext.cs
+++ UnitOfWorkApplication.Model/Entities/UnitOfWorkApplicationDBContext.cs
@@ -15,9 +15,29 @@ namespace UnitOfWorkApplication.Model.Entities
             Database.SetInitializer<UnitOfWorkApplicationDBContext>(null);
         }
 
-        public DbSet<Person> Person { get; set; }
-        public DbSet<Country> Country { get; set; }
+        public DbSet<Car> Car { get; set; }
+
+        public DbSet<CarType> CarType { get; set; }
+
+        public DbSet<Coupon> Coupon { get; set; }
+
+        public DbSet<CouponValueType> CouponValueType { get; set; }
+
+        public DbSet<Driver> Driver { get; set; }
+
+        public DbSet<DriverLocationDetails> DriverLocationDetails { get; set; }
+
+        public DbSet<RideDetails> RideDetails { get; set; }
+
+        public DbSet<RideStatus> RideStatus { get; set; }
+
         public DbSet<User> User { get; set; }
+
+        public DbSet<UserAddress> UserAddress { get; set; }
+
+        public DbSet<UserCoupons> UserCoupons { get; set; }
+
+        public DbSet<UserLocationDetails> UserLocationDetails { get; set; }
 
     }
 }
--- UnitOfWorkApplication.Services/Interfaces/IUserService.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnitOfWorkApplication.Model.Entities;
-
-namespace UnitOfWorkApplication.Services.Interfaces
-{
-    public interface IUserService : IEntityService<User>
-    {
-        User GetById(int Id);
-    }
-}
