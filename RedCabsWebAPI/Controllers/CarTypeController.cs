6fb16ea013530a569f8aeb7d5615b53544c565d0
Akhil Chaudhari <akhil.chaudhari91@live.com>
2015-10-27 10:50:20 +0530

    Added the CarType controller

---
+++ RedCabsWebAPI/Controllers/CarTypeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using UnitOfWorkApplication.Model.Entities;
+using UnitOfWorkApplication.Services.Interfaces;
+
+namespace RedCabsWebAPI.Controllers
+{
+    public class CarTypeController : ApiController
+    {
+        ICarTypeService carTypeService;
+
+        public CarTypeController(ICarTypeService carTypeService)
+        {
+            this.carTypeService = carTypeService;            
+        }
+        public List<CarType> Get()
+        {
+            var carTypes = this.carTypeService.GetAll().ToList();
+           
+            return carTypes;
+        }
+
+        public CarType Get(int id)
+        {
+            var carType = this.carTypeService.GetById(id);
+
+            return carType;
+        }
+    }
+}
